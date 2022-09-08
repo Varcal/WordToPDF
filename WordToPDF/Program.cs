8f7d525d5497d28975b1d7aa7fe101af56cc59bd
Cleber Varçal <cleber.varcal@outlook.com>
2022-09-08 00:27:59 -0300

    Add project files.

---
+++ WordToPDF/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Spire.Doc;
+
+var doc = new Document();
+doc.LoadFromFile("D:\\Users\\varcal\\Desktop\\Eu.docx");
+
+doc.Replace("@nome", "Varçal", true, true);
+
+doc.SaveToFile("D:\\Users\\varcal\\Desktop\\meudoc.pdf", FileFormat.PDF);
+
+var pdf = new MemoryStream();
+doc.SaveToStream(pdf, FileFormat.PDF);
+
+var file = Convert.ToBase64String(pdf.ToArray());
+
+Console.WriteLine(file);
+
+Console.ReadKey();
---
+++ src/WebApp/Controllers/PdfController.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Mime;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Spire.Doc;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfController : ControllerBase
+    {
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public PdfController(IHostEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        [HttpGet]
+        [Route("dowload")]
+        public async Task<IActionResult> Download()
+        {
+            var path = _hostEnvironment.ContentRootPath;
+
+
+            var document = new Document();
+            document.LoadFromFile(@$"{path}/Templates/Eu.docx");
+
+            document.Replace("@nome", "Cleber Varçal", true, true);
+
+            var stream = new MemoryStream();
+            document.SaveToStream(stream, FileFormat.PDF);
+
+            var bytes = stream.ToArray();
+            return File(bytes, MediaTypeNames.Application.Pdf);
+        }
+    }
+}
